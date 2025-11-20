64b2c7644f27b5704f3b54ba77eace2bcb258597
Lennert Ádám <lennertadam@ktch.hu>
2025-11-20 18:25:40 +0100

    Added Models

---
+++ app/Models/Reservation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    
+    
+    public function service(){
+        return $this->belongsTo(service::class);
+    }
+}
