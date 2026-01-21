8a62a6c6c3a9dd7690bd496d2df968328623fdc2
Lennert Ádám <lennertadam@ktch.hu>
2026-01-21 18:16:08 +0100

    Felhasználók hozzáadása seeder-hez

--- database/seeders/User.sql
+++ database/seeders/User.sql
@@ -1,3 +1,5 @@
 INSERT INTO users(username,name,password,role,email)
 VALUES
-("User1","Felhasználó Feri","Aa123","user","Feri@email.lan")
+("User1","Felhasználó Feri","Aa123","barber","Feri@email.lan"),
+("User2","Felhasználó Fülöp","Aa123","admin","Fülöp@email.lan"),
+("User3","Felhasználó Franciska","Aa123","user","Franciska@email.lan")
