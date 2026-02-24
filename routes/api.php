b220e6b5b174af041d89096631a852f9ea59d1e3
Lennert Ádám <lennertadam@ktch.hu>
2026-02-24 18:54:19 +0100

    TestController Törölve

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 */
-
-//TEST
-Route::get("/test",[TestController::class,"test1"]);
-Route::get("/users",[UserController::class,"getUsers"]);
-Route::get("/userId",[TestController::class,"userIdTest"]);
-
 //USER
 Route::post("/register",[UserController::class,"register"]);
 Route::post("/login",[UserController::class,"login"]);
