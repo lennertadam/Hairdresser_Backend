1158bf0c73c73b4eff48a157c55f7908b2bd4e17
Lennert Ádám <lennertadam@ktch.hu>
2026-04-23 18:08:58 +0200

    Dokumentáció áthelyezve

--- database/seeders/Service.sql
+++ database/seeders/Service.sql
@@ -1,6 +1,6 @@
 INSERT INTO services(service, required_time,price)
 VALUES
 ("Haj Vágás",'00:30:00',200),
-("Haj Festés",'00:45:00',500),
-("Borotválás",'00:20:00',1000),
+("Haj Festés",'01:00:00',500),
+("Borotválás",'00:30:00',1000),
 ("Szabadnap","13:00:00",0);
